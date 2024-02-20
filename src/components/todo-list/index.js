fb157cda5e7d60512571745a55faf026d5dd0e29
Melomey <samuelmelomey@gmail.com>
2024-02-20 10:46:20 -0800

    added bootstrap

--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,28 +1,26 @@
 import {useState,useEffect} from "react";
 import {useLocalStorage} from "usehooks-ts";
+
+
 function TodoList(){
-
-
-// let todos = [];
-
 const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
 
- 
-
-// function getTodos()
-//  {
-// //     //Get all todos from local storage and store it.
-//     let todos = JSON.parse(localStorage.getItem("TODO_KEY")) || [];
-// //     //Update React todos
-//     setTodos(todos);
-//  }
-
-// useEffect(getTodos, []);
-
     return(
-        <ul>
+        <ul className="list-group">
             {todos.map(function(todo, index){
-                return <li key={index}> {todo} </li>
+                return (
+                <li className="list-group-item" 
+                key={index}> 
+                <input className="form-check-input me-1"
+                type="checkbox"
+                value=""
+                id={index}/>
+                <label
+                className="form-check-label"
+                for={index}
+                >{todo}</label>
+                </li>
+                );
             })}
         </ul>
        );
